914ac703a6fdb8b69053aa2f23942e3963fc3bf2
Keaton Anthony Murray <murraykeaton422@gmail.com>
2024-11-15 15:15:14 -0500

    modified swal button colors for consistency

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
             text: "You won't be able to revert this!",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
+            confirmButtonColor: "#000",
+            cancelButtonColor: "#000",
             confirmButtonText: "Yes, delete it!"
         }).then((result) => {
             if (result.isConfirmed) {
